f73a162481e8b734eac025555b2db1dcc28bb173
Dementes <julian1dave1@hotmail.com>
2026-01-27 18:14:56 -0500

    feat: habilitar flujos diferenciados para client y seller (Épica 2)

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,13 +56,18 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -70,6 +75,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
