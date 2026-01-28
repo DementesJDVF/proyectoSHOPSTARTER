543eae30c059f60132b2ac5aae991d7f8cbe314b
Dementes <julian1dave1@hotmail.com>
2026-01-28 18:24:44 -0500

    feat: crear app products con modelo de producto (Epica 3)

---
+++ products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from stores.models import Store
+
+
+class Product(models.Model):
+    store = models.ForeignKey(
+        Store,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
