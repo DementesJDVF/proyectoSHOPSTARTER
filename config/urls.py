48d96d4d73f0adbf4c7b2936d6f40bdc1da7d9af
Dementes <julian1dave1@hotmail.com>
2026-01-28 18:39:30 -0500

    feat: permitir a vendedores crear productos desde el dashboard (Epica 3)

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path('users/', include('users.urls')),
     path('stores/', include('stores.urls')),
-
+    path('products/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from .models import Product
+from .forms import ProductForm
+
+
+@login_required
+def product_list(request):
+    if not request.user.is_seller():
+        raise PermissionDenied
+
+    store = getattr(request.user, 'store', None)
+    products = Product.objects.filter(store=store)
+
+    return render(
+        request,
+        'products/product_list.html',
+        {'products': products}
+    )
+
+
+@login_required
+def product_create(request):
+    if not request.user.is_seller():
+        raise PermissionDenied
+
+    store = getattr(request.user, 'store', None)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.store = store
+            product.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+
+    return render(
+        request,
+        'products/product_form.html',
+        {'form': form}
+    )
