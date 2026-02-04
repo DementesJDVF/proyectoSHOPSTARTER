85f151293858b55212926f28031bce22759fc48e
Dementes <julian1dave1@hotmail.com>
2026-02-04 17:50:25 -0500

    puntero

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import cart_add, cart_detail
+from .views import (
+    cart_add,
+    cart_detail,
+    cart_update,
+    cart_remove
+)
 
 urlpatterns = [
-    path('', cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', cart_add, name='cart_add'),
+    path('', cart_detail, name='cart_detail'),
+    path('update/<int:product_id>/', cart_update, name='cart_update'),
+    path('remove/<int:product_id>/', cart_remove, name='cart_remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from products.models import Product
+from django.contrib import messages
 from .cart import Cart
 
 def cart_add(request, product_id):
@@ -9,9 +10,54 @@ def cart_add(request, product_id):
     return redirect('cart_detail')
 
 def cart_detail(request):
-    cart = Cart(request)
+    cart = request.session.get('cart', {})
+    cart_items = []
+
+    for product_id, item in cart.items():
+        product = Product.objects.get(id=product_id)
+        cart_items.append({
+            'product': product,
+            'quantity': item['quantity'],
+            'subtotal': product.price * item['quantity']
+        })
+
     return render(
         request,
         'cart/cart_detail.html',
-        {'cart': cart}
+        {'cart_items': cart_items}
     )
+
+
+def cart_update(request, product_id):
+    cart = request.session.get('cart', {})
+    product = get_object_or_404(Product, id=product_id)
+
+    action = request.POST.get('action')
+
+    if str(product_id) in cart:
+        if action == 'increase':
+            if cart[str(product_id)]['quantity'] < product.stock:
+                cart[str(product_id)]['quantity'] += 1
+            else:
+                messages.warning(
+                    request,
+                    'No hay más stock disponible.'
+                )
+
+        elif action == 'decrease':
+            cart[str(product_id)]['quantity'] -= 1
+            if cart[str(product_id)]['quantity'] <= 0:
+                del cart[str(product_id)]
+
+    request.session['cart'] = cart
+    return redirect('cart_detail')
+
+def cart_remove(request, product_id):
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        del cart[str(product_id)]
+        request.session['cart'] = cart
+
+    return redirect('cart_detail')
+
